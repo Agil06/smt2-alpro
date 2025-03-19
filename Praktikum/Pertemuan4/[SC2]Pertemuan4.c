020713b6cb22d72ca15ef25966f0cf9ba4ac8054
Agil Yudis Wibawa <160165888+Agil06@users.noreply.github.com>
2025-03-20 00:48:52 +0700

    Create [SC2]Pertemuan4.c

---
+++ Praktikum/Pertemuan4/[SC2]Pertemuan4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//MULTIDIMENSION ARRAY
+int main(void){
+    int layar[10][10];
+
+    int layar2[3][3] = {
+        {1, 2, 3},
+        {1, 2, 3},
+        {1, 2, 3}
+    };
+
+    int i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf("%d ", layar2[i][j]); // Corrected printf statement
+        }
+        printf("\n"); // Added to print each row on a new line
+    }
+
+    return 0;
+}
